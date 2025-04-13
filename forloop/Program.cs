aaa66ac87920f9dc936f7f085abd77b73d51ace4
alyorjon <alyor6328@gmail.com>
2025-04-13 18:36:40 +0500

    loops

---
+++ forloop/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+for loop in C#
+for (initialization; condition; increment/decrement)
+{
+    // code to be executed
+}
+*/
+for (int i=0;i<10;i++)
+{
+    Console.WriteLine("Hello World!");
+}
+Console.WriteLine("The loop has ended.");
+
+
+/*
+foreach loop in C#
+foreach (dataType variable in collection)
+{
+    // code to be executed
+}
+// Example: Iterating through an array
+*/
+foreach (int number in new int[] { 1, 2, 3, 4, 5 })
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine("The loop has ended.");
---
+++ whileloop/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+while loop
+*/
+int counter=0;
+while (counter<10)
+{
+    Console.WriteLine("Hello World!");
+    counter++;
+}
+Console.WriteLine("The loop has ended.");
+
+/*
+do while loop
+*/
+int counter2=0;
+do
+{
+    Console.WriteLine("Hello World!");
+    counter2++;
+} while (counter2<10);
+Console.WriteLine("The loop has ended.");
