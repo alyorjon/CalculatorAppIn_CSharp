1096601e40a365340dcf7c26d5d7bce880cd5f02
alyorjon <alyor6328@gmail.com>
2025-04-13 15:03:03 +0500

    init

---
+++ Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter first number: ");
+int firstInput=int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int secondInput=int.Parse(Console.ReadLine());
+Console.WriteLine("Enter operator (+, -, *, /): ");
+string operatorInput=Console.ReadLine();    
+Console.WriteLine(firstInput+secondInput);
